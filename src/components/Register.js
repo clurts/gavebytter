7304ba646c75398cc8e22930bd86f451b93dbb5c
Carsten Lund <clu@rts.dk>
2021-11-21 18:40:55 +0100

    updates

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
 const Login = () => {
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
 import Splash from "../components/Splash";
 import Login from "../components/Login";
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
 	const [splash, setSplash] = useState(true);
-	let navigate = useNavigate();
 
 	useEffect(() => {
 		setTimeout(() => {
